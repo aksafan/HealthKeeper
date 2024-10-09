ec29095539347c0ca28b1ba3653ed7988af4bfa6
aksafan <subzero_ck@ukr.net>
2024-10-09 00:24:22 -0500

    Add auto date update to LadTest model and belonged associations

--- app/models/lab_test.rb
+++ app/models/lab_test.rb
@@ -2,7 +2,7 @@ class LabTest < ApplicationRecord
   belongs_to :user
   belongs_to :biomarker
   belongs_to :reference_range
-  belongs_to :recordable, polymorphic: true
+  belongs_to :recordable, polymorphic: true, touch: true
 
   validates :value, presence: true, numericality: true
   validates :unit, presence: true
